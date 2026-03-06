23bf393e1341f151921186d5294b5ff4e9469a00
sRatov <sratsov@coca-cola.com>
2026-03-06 17:30:02 +0200

    tests re-built

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 "use strict";
-//write npm command line to install mocha
-//command to run this test file
-//mocha test.ts
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -35,36 +32,98 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert = __importStar(require("assert"));
 const http = __importStar(require("http"));
+const makeRequest = (path) => {
+    return new Promise((resolve, reject) => {
+        const req = http.get(`http://localhost:3000${path}`, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk.toString();
+            });
+            res.on('end', () => resolve(data));
+        });
+        req.on('error', reject);
+    });
+};
 describe('Node Server', () => {
-    it('should return "key not passed" if key is not passed', (done) => {
-        http.get('http://localhost:3000/get', (res) => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                assert.strictEqual(data, 'key not passed');
-                done();
-            });
-        });
-    });
-    it('should return "hello world" if key is equal to world', (done) => {
-        http.get('http://localhost:3000/get?key=world', (res) => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                assert.strictEqual(data, 'hello world');
-                done();
-            });
-        });
-    });
-    //add test to check validatephoneNumber
-    //write test to validate validateSpanishDNI
-    //write test for returnColorCode red should return code #FF0000
-    //write test for daysBetweenDates
+    it('should return "key not passed" if key is not passed', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/get');
+        assert.strictEqual(data, 'key not passed');
+    }));
+    it('should return "hello world" if key is equal to world', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/get?key=world');
+        assert.strictEqual(data, 'hello world');
+    }));
+    it('should return number of days between two dates', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/daysbetweendates?date1=2024-01-01&date2=2024-01-11');
+        assert.strictEqual(data, '10');
+    }));
+    it('should return valid for a valid Spanish phone number', () => __awaiter(void 0, void 0, void 0, function* () {
+        const encodedPhoneNumber = encodeURIComponent('+34666777888');
+        const data = yield makeRequest(`/validatephonenumber?phoneNumber=${encodedPhoneNumber}`);
+        assert.strictEqual(data, 'valid');
+    }));
+    it('should return invalid for an invalid Spanish phone number', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/validatephonenumber?phoneNumber=666777888');
+        assert.strictEqual(data, 'invalid');
+    }));
+    it('should return valid for a valid Spanish DNI', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/validatespanishdni?dni=12345678Z');
+        assert.strictEqual(data, 'valid');
+    }));
+    it('should return invalid for an invalid Spanish DNI', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/validatespanishdni?dni=12345678A');
+        assert.strictEqual(data, 'invalid');
+    }));
+    it('should return the hex code for red', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/returncolorcode?color=red');
+        assert.strictEqual(data, '#FF0000');
+    }));
+    it('should parse a URL and return the parsed host', () => __awaiter(void 0, void 0, void 0, function* () {
+        const encodedUrl = encodeURIComponent('https://example.com:8080/path?a=1#hash');
+        const data = yield makeRequest(`/parseurl?someurl=${encodedUrl}`);
+        const parsed = JSON.parse(data);
+        assert.strictEqual(parsed.parsedHost, 'example.com:8080');
+        assert.strictEqual(parsed.protocol, 'https:');
+        assert.strictEqual(parsed.path, '/path');
+    }));
+    it('should return files from the current directory', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/listfiles');
+        const files = JSON.parse(data);
+        assert.ok(files.includes('nodeserver.ts'));
+        assert.ok(files.includes('test.ts'));
+    }));
+    it('should return lines containing Fusce from full text file read', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/getfulltextfile');
+        const lines = JSON.parse(data);
+        assert.ok(lines.length > 0);
+        assert.ok(lines.every((line) => line.includes('Fusce')));
+    }));
+    it('should return lines containing Fusce using line-by-line file read', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/getlinebylinefromttextfile');
+        const lines = JSON.parse(data);
+        assert.ok(lines.length > 0);
+        assert.ok(lines.every((line) => line.includes('Fusce')));
+    }));
+    it('should return process memory consumption in GB with 2 decimals', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/calculatememoryconsumption');
+        assert.ok(/^\d+\.\d{2}$/.test(data));
+    }));
+    it('should return a random European country and ISO code', () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield makeRequest('/randomeuropeancountry');
+        const country = JSON.parse(data);
+        assert.ok(country.country.length > 0);
+        assert.ok(/^[A-Z]{2}$/.test(country.isoCode));
+    }));
 });
